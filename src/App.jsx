884924b682b57b0744ca6f0f12ded00ad2d3707e
korelin_ks <johnny_85@list.ru>
2026-09-10 15:28:38 +0500

    setInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css'
 import { useState } from 'react'
 function App() {
-  const [tasks, setTasks] = useState([]) 
-  const [inputValue, setInputValue] = useState("")
+  const [tasks, setTasks] = useState([]); 
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTask = () => {
+    setTasks([...tasks, {id: Date.now(), text: inputValue}]);
+    setInputValue("")
+  }
+
+
   return <div className='App'>
     <h1>Мой список дел</h1>
     <div className='inputContainer'>
@@ -11,6 +18,7 @@ function App() {
       onChange={(e) => setInputValue(e.target.value)}
       placeholder='Введите задачу...'
       />
+      <button onClick={handleAddTask}>Добавить</button>
     </div>
     <p>Задач: {tasks.length}</p>
   </div>
