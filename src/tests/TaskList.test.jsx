cf3e703b76f27b4532786de6ccbb1e94bab767ab
korelin_ks <johnny_85@list.ru>
2026-09-18 12:12:20 +0500

    tests for taskList

--- src/tests/TaskList.test.jsx
+++ src/tests/TaskList.test.jsx
@@ -1,3 +1,34 @@
-test('simple stub test', () => {
-  expect(true).toBe(true);
+import { render, screen } from "@testing-library/react";
+import TaskList from "../TaskList";
+
+describe("TaskList component", () => {
+  test("Отображение списка задач", () => {
+    const tasks = [
+      { id: 1, text: "Задача 1" },
+      { id: 2, text: "Задача 2" },
+      { id: 3, text: "Задача 3" },
+    ];
+    
+    render(
+      <TaskList 
+        tasks={tasks} 
+        onDeleteTask={() => {}} 
+      />
+    );
+
+    expect(screen.getByText("Задача 1")).toBeInTheDocument();
+    expect(screen.getByText("Задача 2")).toBeInTheDocument();
+    expect(screen.getByText("Задача 3")).toBeInTheDocument();
+  });
+
+  test("Пустой список задач", () => {
+    render(
+      <TaskList 
+        tasks={[]} 
+        onDeleteTask={() => {}} 
+      />
+    );
+
+    expect(screen.queryByText("Задача 1")).not.toBeInTheDocument();
+  });
 });
