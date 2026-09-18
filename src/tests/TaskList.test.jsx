29a6e1ee98bf3c467e49a98319145e9daeed2fb0
korelin_ks <johnny_85@list.ru>
2026-09-18 14:48:21 +0500

    comments 4

--- src/tests/TaskList.test.jsx
+++ src/tests/TaskList.test.jsx
@@ -16,8 +16,13 @@ describe("TaskList component", () => {
       />
     );
 
+    // toBeInTheDocument – проверяет, что элемент с текстом "Задача 1" присутствует в DOM
     expect(screen.getByText("Задача 1")).toBeInTheDocument();
+
+    // toBeInTheDocument – проверяет, что элемент с текстом "Задача 2" присутствует в DOM
     expect(screen.getByText("Задача 2")).toBeInTheDocument();
+
+    // toBeInTheDocument – проверяет, что элемент с текстом "Задача 3" присутствует в DOM
     expect(screen.getByText("Задача 3")).toBeInTheDocument();
   });
 
@@ -29,6 +34,10 @@ describe("TaskList component", () => {
       />
     );
 
+
+    // not.toBeInTheDocument – проверяет, что элемент с текстом "Задача 1" отсутствует в DOM.
+    // Важно: используется queryByText вместо getByText, так как getByText выбросил бы ошибку, 
+    // если элемент не найден, а queryByText возвращает null, что позволяет корректно проверить отсутствие элемента.
     expect(screen.queryByText("Задача 1")).not.toBeInTheDocument();
   });
 });
